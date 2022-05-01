039a7fdff2ba077ff41c5a767b7e7831f46cd793
luisdaniel1709 <103487536+luisdaniel1709@users.noreply.github.com>
2022-05-01 08:53:23 -0400

    Update main.py

--- main.py
+++ main.py
@@ -555,7 +555,8 @@ def onmessage(update,bot:ObigramClient):
         thread.store('msg',message)
 
         if '/start' in msgText:
-            message = bot.sendMessage(update.message.chat.id,'🦾')
+            start_msg = bot.sendMessage(update.message.chat.id,'🦾')
+            bot.editMessageText(message,start_msg)
             start_msg = '   🌟𝔹𝕆𝕋 𝕀ℕ𝕀ℂ𝕀𝔸𝔻𝕆🌟\n'
             start_msg+= '࿇ ══━━━━✥◈✥━━━━══ ࿇\n'
             start_msg+= '🤖Hola @' + str(username)+'\n'
@@ -565,7 +566,6 @@ def onmessage(update,bot:ObigramClient):
             start_msg+= '/help\n'
             start_msg+= '/about\n\n'
             start_msg+= '😁𝚀𝚞𝚎 𝚍𝚒𝚜𝚏𝚛𝚞𝚝𝚎 𝚐𝚛𝚊𝚗𝚍𝚎𝚖𝚎𝚗𝚝𝚎 𝚜𝚞 𝚎𝚜𝚝𝚊𝚍𝚒𝚊😁.\n'
-            bot.editMessageText(message,start_msg)
             message = bot.sendMessage(update.message.chat.id,'🦾')
         elif '/files' == msgText and user_info['cloudtype']=='moodle':
              proxy = ProxyCloud.parse(user_info['proxy'])
